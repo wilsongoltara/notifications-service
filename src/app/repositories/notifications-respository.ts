a14e0fcacb93dd01cb5c1ba0d527a74f97b5c4e0
wilsongoltara <wilson.goltara15@gmail.com>
2022-12-26 19:13:56 -0300

    [FEAT] Add entites cancel-notfication

--- src/app/repositories/notifications-respository.ts
+++ src/app/repositories/notifications-respository.ts
@@ -2,4 +2,6 @@ import { Notification } from '../entities/notifications';
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -4,7 +4,15 @@ import { NotificationsRepository } from '@app/repositories/notifications-resposi
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
