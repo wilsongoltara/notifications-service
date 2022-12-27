6fd7471fcd92af12ac3a7d095ade6c00bcd1ea0f
wilsongoltara <wilson.goltara15@gmail.com>
2022-12-26 23:48:02 -0300

    [FEAT] Implements methods for InMemoryNotification

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -5,7 +5,15 @@ export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
   async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
   }
 
   async create(notification: Notification) {
@@ -13,6 +21,12 @@ export class InMemoryNotificationRepository implements NotificationsRepository {
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
   }
 }
